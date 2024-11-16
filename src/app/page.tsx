3fad8a087dc1c46a115d71ee0144a3d90b4c65fd
Faryal Abbasi <faryalabbasi88@gmail.com>
2024-11-16 21:23:13 +0500

    Adding a blog website

---
+++ src/app/page.tsx
@@ -0,0 +1,18 @@
+import Hero from "./components/hero";
+import AboutPage from "./components/about";
+import BlogPage from "./blog/blogpage";
+import Contact from "./components/contact"
+import Footer from "./components/footer";
+
+
+export default function Home() {
+  return (
+    <div>
+      <Hero />
+      <AboutPage/>
+      <BlogPage/>
+      <Contact/>
+      <Footer/>
+    </div>
+  );
+}
